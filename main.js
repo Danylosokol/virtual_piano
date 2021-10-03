9ad9b893bfac068587febb6741d9c6b75efb959f
Danylosokol <dansokol677@gmail.com>
2021-10-03 08:23:10 +0200

    add sound to all piano keys

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
 document.addEventListener("keydown", function(event){
     if(event.code === "KeyA"){
-        let audio = new Audio("");
-        console.log("The 'A' key is pressed")
+        let audio = new Audio("white_keys/A.mp3");
+        let result = audio.play();
+        if(result === undefined){
+            console.log("The 'A' key is pressed")
+        }
     }
     else if(event.code === "KeyS"){
-        console.log("The 'S' key is pressed");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/S.mp3";
+        let result = audio.play();
+        if(result === undefined){
+            console.log("The 'S' key is pressed");
+        }
     }
     else if(event.code === "KeyD"){
-        console.log("The 'D' key is pressed");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/D.mp3"
+        let result = audio.play();
+        if(result === undefined){
+            console.log("The 'D' key is pressed");
+        }
     }
     else if(event.code === "KeyF"){
-        console.log("The 'F' key is pressed");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/F.mp3"
+        let result = audio.play();
+        if(result === undefined) {
+            console.log("The 'F' key is pressed");
+        }
     }
     else if(event.code === "KeyG"){
-        console.log("The 'G' key is pressed");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/G.mp3"
+        let result = audio.play();
+        if(result === undefined) {
+            console.log("The 'G' key is pressed");
+        }
     }
     else if(event.code === "KeyH"){
-        console.log("The 'H' key is pressed");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/H.mp3";
+        let result = audio.play();
+        if(result === undefined){
+            console.log("The 'H' key is pressed");
+        }
     }
     else if(event.code === "KeyJ"){
-        console.log("The 'J' key is pressed");
+        let audio = new Audio("white_keys/J.mp3");
+        let result = audio.play();
+        if(result === undefined) {
+            console.log("The 'J' key is pressed");
+        }
     }
     else{
         console.log("Unbound key was pressed");
